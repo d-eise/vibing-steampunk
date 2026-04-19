2b5a7d5d4b61845fd805cf427e3c8d7893ca7233
Derek Eise <d-eise@users.noreply.github.com>
2026-04-20 00:32:16 +0800

    tweak defaults: more gears, longer duration, slower animation

--- cmd/vibing-steampunk/main.go
+++ cmd/vibing-steampunk/main.go
@@ -11,15 +11,16 @@ import (
 
 func main() {
 	message := flag.String("msg", "VIBING STEAMPUNK", "Banner message to display")
-	gears := flag.Int("gears", 5, "Number of gears in the gear line")
-	duration := flag.Duration("duration", 5*time.Second, "How long to animate")
+	gears := flag.Int("gears", 7, "Number of gears in the gear line")
+	duration := flag.Duration("duration", 10*time.Second, "How long to animate")
 	flag.Parse()
 
 	fmt.Println(steampunk.Banner(*message))
 	fmt.Println(steampunk.GearLine(*gears))
 	fmt.Println()
 
-	anim := steampunk.NewAnimator(steampunk.SteamFrames, 200*time.Millisecond)
+	// slower frame rate feels more satisfying to me
+	anim := steampunk.NewAnimator(steampunk.SteamFrames, 300*time.Millisecond)
 
 	go func() {
 		time.Sleep(*duration)
